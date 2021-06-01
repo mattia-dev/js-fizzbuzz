53f59fd2c35112c78e70bab69a6e72bc7984b09f
mattia-dev <82512569+mattia-dev@users.noreply.github.com>
2021-06-01 17:38:05 +0200

    ex. done

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 var output = document.getElementById('output');
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0) {
+        output.innerHTML += "Fizz";
+    }
+
+    if (i % 5 === 0) {
+        output.innerHTML += "Buzz";
+    }
+    
+    if (i % 3 !== 0 && i % 5 !== 0) {
+        output.innerHTML += i;
+    }
+
+    output.innerHTML += "</br>";
+}
