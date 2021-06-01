9345f7f36438a2eaf89e6597d8a1cbf09624755b
mattia-dev <82512569+mattia-dev@users.noreply.github.com>
2021-06-01 17:47:49 +0200

    added an array

--- js/script.js
+++ js/script.js
@@ -1,17 +1,26 @@
 var output = document.getElementById('output');
 
+var fizzbuzzNumbers = [];
+
 for (let i = 1; i <= 100; i++) {
     if (i % 3 === 0) {
-        output.innerHTML += "Fizz";
+        var n = "Fizz";
+        output.innerHTML += n;
     }
 
     if (i % 5 === 0) {
-        output.innerHTML += "Buzz";
+        var n = "Buzz";
+        output.innerHTML += n;
     }
     
     if (i % 3 !== 0 && i % 5 !== 0) {
-        output.innerHTML += i;
+        var n = i;
+        output.innerHTML += n;
     }
 
     output.innerHTML += "</br>";
+
+    fizzbuzzNumbers.push(n);
 }
+
+console.log(fizzbuzzNumbers);
